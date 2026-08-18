bdc8c5f2cf886973e5f31553e2d1628e4277803b
Richard Dugbartey <richard.dugbartey@gmail.com>
2026-08-18 14:30:24 +0000

    Update e-plantShopping

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -9,27 +9,54 @@ const CartItem = ({ onContinueShopping }) => {
 
   // Calculate total amount for all products in the cart
   const calculateTotalAmount = () => {
- 
+    let total = 0;
+
+  items.forEach((item) => {
+    const quantity = item.quantity;
+    const cost = parseFloat(item.cost.substring(1));
+
+    total += quantity * cost;
+  });
+
+  return total;
   };
 
   const handleContinueShopping = (e) => {
-   
+    onContinueShopping(e);
   };
 
-
+  const handleCheckoutShopping = (e) => {
+  alert('Functionality to be added for future reference');
+  };
 
   const handleIncrement = (item) => {
+    dispatch(
+        updateQuantity({
+            name: item.name,
+            quantity: item.quantity + 1
+        })
+    );
   };
 
   const handleDecrement = (item) => {
-   
+    if (item.quantity > 1) {
+        dispatch(
+            updateQuantity({
+                name: item.name,
+                quantity: item.quantity - 1
+            })
+        );
+    }
   };
 
   const handleRemove = (item) => {
+    dispatch(removeItem(item.name));
   };
 
   // Calculate total cost based on quantity for an item
   const calculateTotalCost = (item) => {
+    const cost = parseFloat(item.cost.substring(1));
+    return cost * item.quantity;
   };
 
   return (
